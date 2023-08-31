e9cde8af4aef6900ef947ac0b7350d4b13c4d22f
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-08-31 20:43:36 -0300

    initial commit

---
+++ api/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const livros = require("./livros");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send(`<h1> <a href="http://localhost:${port}/introducao"> Oi Pessoal. Seja Bem-vindo! </a> </h1>`);
+});
+
+app.get("/introducao", (req, res) => {
+    res.send(`<h2> <a href="http://localhost:${port}/transfere"> Introdução ao Express </a> </h2>`);
+});
+
+app.post("/filmes", (req, res) => {
+    const {titulo, genero} = req.body;
+    res.send(`Filme: ${titulo} - Gênero: ${genero}, recebido...`);
+});
+
+const log = (req, res, next) => {
+    console.log(`Acessado em ${new Date()}`);
+    next();
+};
+app.get('/transfere', log, (req, res) => {
+    res.send("Valor transferido com sucesso!");
+});
+
+app.use('/livros', livros);
+
+app.listen(port, () => {
+    console.log(`🌟Servidor🌠 rodando em: http://localhost:${port}`);
+});
