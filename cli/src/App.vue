d14aafed9fbef4a8c59d3055c259a6530abb068b
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-09-01 18:28:37 -0300

    fronte

---
+++ cli/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<template>
+  <div class="center">
+    <h1>Hello Word</h1>
+  </div>
+</template>
+
+<style scoped>
+.center {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.active {
+  border: 2px red solid;
+}
+</style>
